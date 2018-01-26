a85815e7f8d5aead801bd420fd429e2595d1c007
Aaron Ritter <aaron-ritter@idexx.com>
2018-01-25 18:01:05 -0600

    Don't read past the end of a Resource array when trying to realign the DataReader

--- java/src/org/boris/pecoff4j/io/ResourceParser.java
+++ java/src/org/boris/pecoff4j/io/ResourceParser.java
@@ -321,7 +321,11 @@ public class ResourceParser {
 
 	private static int alignDataReader(IDataReader dr) throws IOException {
 		int off = (4 - (dr.getPosition() % 4)) % 4;
-		dr.skipBytes(off);
+		for (int i = 0; i < off; i++) {
+			if (!dr.hasMore())
+				break;
+			dr.skipBytes(1);
+		}
 		return off;
 	}
 }
