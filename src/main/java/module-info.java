b351f3536ca4e46a5a484b709e79697d9770fed1
Concision <contact@concision.me>
2020-09-28 15:49:02 -0700

    feat: added module-info with name 'com.kichik.pecoff4j'

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.kichik.pecoff4j {
+    requires java.base;
+
+    exports com.kichik.pecoff4j;
+    exports com.kichik.pecoff4j.asm;
+    exports com.kichik.pecoff4j.constant;
+    exports com.kichik.pecoff4j.io;
+    exports com.kichik.pecoff4j.resources;
+    exports com.kichik.pecoff4j.util;
+}
